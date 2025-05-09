d6d2ba643593dfea2b19db71eee43100c7fbe92e
Jean Sacren <sakiwit@gmail.com>
2025-05-08 23:45:29 -0600

    Add Categories Blade view
    Use the current nav component to build the Categories view that is in
    alignment with the full view.

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app title="Categories">
+    <div class="bg-gray-100 max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="h-16 flex items-center">
+            <div class="sm:ml-6">
+                <h1 class="text-2xl font-bold text-gray-800 pl-3">Categories</h1>
+            </div>
+        </div>
+    </div>
+</x-layouts.app>
