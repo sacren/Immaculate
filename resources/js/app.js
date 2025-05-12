2083e4d4e7fed77b7edfd1a5ad6f98c4b79e7ff1
Jean Sacren <sakiwit@gmail.com>
2025-05-11 21:44:57 -0600

    Set up installed Alpine.js for use
    Load Alpine.js from dependencies.  Make Alpine globally available
    (window.Alpine) so it can be used in HTML.  Defer executing till the
    entire DOM is loaded before initializing Alpine components.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+// Defer Alpine.start() until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    Alpine.start();
+});
