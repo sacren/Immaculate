96b4dacde487eb07c5747431b2abba48f8b93678
Jean Sacren <sakiwit@gmail.com>
2025-05-12 23:21:55 -0600

    Add named slot for Dashboard header
    Build the Dashboard header using named slot.  The slot definition will
    be injected into the rendering of the header slot in the base layout.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,10 @@
 <x-layouts.app title="Dashboard">
     <!-- Header -->
-    <div class="bg-gray-100 max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div class="h-16 flex items-center">
-            <div class="sm:ml-6">
-                <h1 class="text-2xl font-bold text-gray-800 pl-3">Dashboard</h1>
-            </div>
-        </div>
-    </div>
+    <x-slot name="header">
+        <h1 class="text-2xl font-bold text-gray-800">
+            {{ __('Dashboard') }}
+        </h1>
+    </x-slot>
 
     <!-- Main Content -->
     <div class="bg-white shadow-md rounded-lg p-6">
