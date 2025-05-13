eb6cdfc549dfdbf170f37fb20e7c6b51faa5528a
Jean Sacren <sakiwit@gmail.com>
2025-05-12 23:49:36 -0600

    Add named slot for Categories header
    Build the Categories header using named slot.  The slot definition will
    be injected into the rendering header slot in the base layout.

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,9 +1,15 @@
 <x-layouts.app title="Categories">
-    <div class="bg-gray-100 max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div class="h-16 flex items-center">
-            <div class="sm:ml-6">
-                <h1 class="text-2xl font-bold text-gray-800 pl-3">Categories</h1>
-            </div>
+    <!-- Header -->
+    <x-slot name="header">
+        <h1 class="text-2xl font-bold text-gray-800">
+            {{ __('Categories') }}
+        </h1>
+    </x-slot>
+
+    <!-- Main Content -->
+    <div class="bg-white shadow-md rounded-lg p-6">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mt-6 mb-6">
+            <p class="text-gray-600">This is your categories content area.</p>
         </div>
     </div>
 </x-layouts.app>
