78ef978ed5c08235d29554034f3578b4ef273c96
Jean Sacren <sakiwit@gmail.com>
2025-05-15 15:30:49 -0600

    Fix warning of multiple instances of Alpine running
    Livewire v3+ includes Alpine.js automatically (bundled with Livewire's
    JavaScript).  By manually installing Alpine.js via npm install alpinejs,
    we're creating a second instance.  These two instances conflict with
    each other.
    
    The easiest solution is to remove manual Alpine.js installation.
    
    $ sail npm uninstall alpinejs
    
    removed 3 packages, and audited 81 packages in 564ms
    
    Next remove the Alpine.js import from the app.js and use Livewire's
    bundled Alpine.js instead.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,1 @@
 import './bootstrap';
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-// Defer Alpine.start() until the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-    Alpine.start();
-});
