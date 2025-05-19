808927734d6df93f2642d9d327ba03f6d75d7e10
Jean Sacren <sakiwit@gmail.com>
2025-05-19 10:39:26 -0600

    Use wire:model.defer for updates
    Livewire uses morphing to update parts of the DOM after an action like
    typing in an input and tabbing away.
    
    The wire:target tells Livewire to re-render the block whenever the $name
    property changes.
    
    But Livewire doesn’t know why or how $name changed unless the change was
    caused by something with a wire: directive.  Use wire:model.defer to
    update the back-end $name property when input loses focus.
    
    Putting both the input and the output ({{ $name }}) inside the same
    wire:target block makes it crystal clear to Livewire what part of the
    page needs to be updated when $name changes.  The connection between the
    input and the target isn't direct and it is Livewire's diffing engine
    that decides what gets updated.  But wrapping them together makes it
    predictable and easier to debug.
    
    Use wire:change="$refresh" to force Livewire to re-render the component
    and update any wire:target sections.

--- resources/views/livewire/make-plan.blade.php
+++ resources/views/livewire/make-plan.blade.php
@@ -1,6 +1,6 @@
-<div class="text-gray-600">
+<div class="text-gray-600" wire:target="name">
     {{ $name }}
 
-    <input type="text" wire:model.live="name"
+    <input type="text" wire:model.defer="name" wire:change="$refresh"
         class="px-3 py-1 text-sm border border-green-500 rounded focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
 </div>
