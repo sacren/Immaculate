0a5465521135b95ca0d050242033900dbfa62766
Jean Sacren <sakiwit@gmail.com>
2025-05-18 23:42:08 -0600

    Use wire:model.live for instant updates
    Livewire will re-render the whole component and everything tied to $name
    will update immediately.

--- resources/views/livewire/make-plan.blade.php
+++ resources/views/livewire/make-plan.blade.php
@@ -1,3 +1,6 @@
 <div class="text-gray-600">
     {{ $name }}
+
+    <input type="text" wire:model.live="name"
+        class="px-3 py-1 text-sm border border-green-500 rounded focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
 </div>
