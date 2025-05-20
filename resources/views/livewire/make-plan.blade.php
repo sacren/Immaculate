38097bfc2415ce1788cfd8eb668fad31636f88e0
Jean Sacren <sakiwit@gmail.com>
2025-05-19 18:32:13 -0600

    Add wire:model.lazy for updates
    The wire:model.lazy is used with <input> to control when Livewire sends
    data to the server.  It only updates the server when the input tag loses
    focus (i.e., user clicks away or presses Tab), similar to native HTML
    lazy behavior.  This also avoids sending updates on every keystroke like
    input event, which improves performance.
    
    The {{ $name }} is only updated when the user finishes typing and leaves
    the field.
    
    On the other hand, the wire:model.defer delays the update to the server
    until after Livewire has processed all other actions currently
    happening.
    
    With wire:model.defer, the {{ $name }} will be updated on key press, but
    the change to the backend is queued and executed after Livewire finishes
    processing any pending actions.
    
    So even though the input triggers an update on each key press, Livewire
    defers syncing that value to the server until it is ready.

--- resources/views/livewire/make-plan.blade.php
+++ resources/views/livewire/make-plan.blade.php
@@ -1,6 +1,11 @@
 <div class="text-gray-600" wire:target="name">
     {{ $name }}
 
+    <!-- Sends update after all current actions are handled -->
     <input type="text" wire:model.defer="name" wire:change="$refresh"
+        class="px-3 py-1 text-sm border border-green-500 rounded focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
+
+    <!-- Updates server only when user leaves the field -->
+    <input type="text" wire:model.lazy="name" wire:change="$refresh"
         class="px-3 py-1 text-sm border border-green-500 rounded focus:ring-blue-500 focus:border-blue-500 focus:outline-none">
 </div>
