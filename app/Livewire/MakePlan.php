9afc739b8fdf563afcbdd6c546c6d1731edc0a36
Jean Sacren <sakiwit@gmail.com>
2025-06-04 21:28:03 -0600

    Add mount method to Livewire component class
    The mount method is a life cycle hook that runs once when the component
    is first loaded.  It’s used to initialize properties, fetch data from
    the database or API and set default values based on request parameters.
    
    When the page loads and Livewire initializes this component, the mount
    method runs before rendering.  It sets $this->name to 'Mounting the
    plan.' instead of the default "We're making a plan.".
    
    Any public property initialized at the class level (like $name) will be
    overridden if we set it again inside mount().

--- app/Livewire/MakePlan.php
+++ app/Livewire/MakePlan.php
@@ -7,10 +7,18 @@ use Livewire\Component;
 class MakePlan extends Component
 {
     public $name = "We're making a plan.";
-
     public $shout = false;
-
     public $greetings = ['Hello!'];
+
+    /**
+     * Mount the component.
+     *
+     * @return void
+     */
+    public function mount(): void
+    {
+        $this->name = 'Mounting the plan.';
+    }
 
     public function resetName($name = 'The plan is ready.'): void
     {
