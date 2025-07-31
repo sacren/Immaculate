12a5a412966216d0d2cdc6c1c34d0a9367e132c0
Jean Sacren <sakiwit@gmail.com>
2025-07-31 12:04:08 -0600

    Use now() in lieu of time()
    The time() call returns a Unix timestamp like 1719543210.  To improve on
    that, show a readable time.
    
    In Livewire Dummy class, we can add extra logic if needed.  But for now
    method, it's not required because Blade re-renders it each time.  So we
    don’t need to pass now() as a public property because now() in Blade is
    evaluated at render time.
    
    But for simple cases, wire:click="$refresh" is perfect.
    
    The wire:click attribute tells Livewire to send an AJAX request when the
    button is clicked.
    
    The $refresh value is a magic action that tells Livewire to re-render
    the entire component.

--- resources/views/livewire/dummy.blade.php
+++ resources/views/livewire/dummy.blade.php
@@ -1,5 +1,5 @@
 <div class="p-6 bg-blue-100 text-gray-800 rounded-lg shadow-md">
-    {{ __('The current time is') }} {{ time() }}
+    {{ __('The current time is') }} {{ now() }}
 
     <button wire:click="$refresh"
         class="px-4 py-2 bg-red-500 text-white text-sm font-medium rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">Refresh</button>
