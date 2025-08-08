cb70a07655d4df082637dbc6da180672a2116f92
Jean Sacren <sakiwit@gmail.com>
2025-08-07 20:52:25 -0600

    Build the layout for appending todo list

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,9 +1,24 @@
 <div>
     <h3 class="text-lg font-semibold text-gray-700 mb-2">Todo List</h3>
 
-    <ul class="list-disc pl-4 space-y-1">
-        @foreach ($todos as $todo)
-            <li class="text-gray-800">{{ $todo }}</li>
-        @endforeach
-    </ul>
+    <div class="flex flex-wrap gap-2 items-center">
+        <div class="flex-1 min-w-0 max-w-md">
+            <input type="text"
+                class="w-full px-3 py-2 bg-white border border-gray-300
+                rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
+        </div>
+        <button type="button"
+            class="bg-blue-500 hover:bg-blue-600 text-white
+            font-medium px-4 py-2 rounded transition">
+            {{ __('Add') }}
+        </button>
+    </div>
+
+    <div class="mt-4">
+        <ul class="list-disc pl-4 space-y-1">
+            @foreach ($todos as $todo)
+                <li class="text-gray-800">{{ $todo }}</li>
+            @endforeach
+        </ul>
+    </div>
 </div>
