e4d23b82ca3d83666638f0ea7ae73a5586d4404d
Jean Sacren <sakiwit@gmail.com>
2025-08-18 12:30:15 -0600

    Build PostPolicy for AuthServiceProvider
    Use artisan to generate policy scaffolding.
    
    $ sail artisan make:policy --model=Post -- PostPolicy
    
       INFO  Policy [app/Policies/PostPolicy.php] created successfully.
    
    Use the default policy for AuthServiceProvider.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,10 +2,21 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
+use App\Policies\PostPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Post::class => PostPolicy::class,
+    ];
+
     /**
      * Register services.
      */
@@ -19,6 +30,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
     }
 }
